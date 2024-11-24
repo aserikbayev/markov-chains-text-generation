9fe8bc9dc7d65ffbe13a45be52becf0d39f766e2
Azat Serikbayev <125980868+aserikbayev@users.noreply.github.com>
2024-11-25 01:13:39 +0500

    🎨 Improve structure of the code
    It was painful to look at two years after.

--- Program.cs
+++ Program.cs
@@ -1,111 +1,62 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using System.Text.Json;
+﻿using System.Text;
 using System.Text.RegularExpressions;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace MarkovChain;
+namespace MarkovChainSentences;
 
-public class MarkovChainSentences
+public class TextGenerator
 {
   private readonly static Random random = new();
-  private readonly Dictionary<string, List<string>> bigrams = new();
-  private readonly List<string> endWords = new();
+  private Index index = new Index();
 
   public static void Main()
   {
-    new MarkovChainSentences().Run();
-  }
+    var generator = TextGenerator.From("input.txt");
 
-  public void Run()
-  {
-    const string inputFileName = "input.txt";
+    var text = generator.GenerateParagraphs(3, 5).ToString().Trim();
 
-    this.Initialize(inputFileName);
-
-    string text = MakeText().ToString();
     Console.WriteLine(text);
   }
 
-  private void Initialize(string filename)
+  public static TextGenerator From(string path)
   {
-    using var sr = new StreamReader(filename);
-    string content = sr.ReadToEnd();
-    string[] wordSeparators = new string[] { "!", "\"", "#", "$", "%", "&", "'", ",", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", "(", ")", " ", Environment.NewLine };
-    var words = content.ToLower().Split(wordSeparators, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-
-    for (int i = 0; i < words.Length - 1; i++)
-    {
-      List<string> nextWords = bigrams.GetValueOrDefault(words[i], new());
-      nextWords.Add(words[i + 1]);
-      bigrams[words[i]] = nextWords;
-    }
-
-    GetEndWords(content);
+    var generator = new TextGenerator();
+    generator.index.IndexFile(path);
+    return generator;
   }
 
-  private void GetEndWords(string content)
+  public StringBuilder GenerateParagraphs(int min = 1, int? max = default, string? initialWord = default)
   {
-    string[] sentenceSeparators = new string[] { "!", ".", "?", ";" };
-    Regex rx = new(@"\b(?<word>\w+)[!.?;]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-    MatchCollection matches = rx.Matches(content);
-
-    endWords.AddRange(matches.Select(match => match.Groups["word"].Value));
-  }
-
-  private StringBuilder MakeText()
-  {
-    var sb = MakeParagraph();
-    for (int i = 0; i < random.Next(2, 5); i++)
+    max ??= min;
+    var sb = new StringBuilder();
+    for (int i = 0; i < random.Next(min, max.Value); i++)
     {
-      sb.Append(Environment.NewLine).Append(Environment.NewLine).Append(MakeParagraph());
+      sb.Append(GenerateParagraph(initialWord)).Append(Environment.NewLine).Append(Environment.NewLine);
     }
-
     return sb;
   }
 
-  private StringBuilder MakeParagraph()
+  public StringBuilder GenerateParagraph(string? initialWord = default)
   {
-    var sb = MakeSentence(GetRandomWord());
-    // Generate 2 to 10 sentences.
+    var sb = new StringBuilder();
     for (int i = 0; i < random.Next(2, 10); i++)
     {
-      string initialWord = GetRandomWord();
-      sb.Append(' ').Append(MakeSentence(initialWord));
+      sb.Append(GenerateSentence(initialWord)).Append(' ');
     }
-
     return sb;
   }
 
-  private string GetRandomWord()
+  public StringBuilder GenerateSentence(string? initialWord = default)
   {
-    int randomWordIndex = random.Next(bigrams.Count);
-    string randomWord = bigrams.ElementAt(randomWordIndex).Key;
-    return randomWord;
-  }
+    initialWord ??= index.RandomToken();
 
-  private StringBuilder MakeSentence(string initialWord)
-  {
-    if (initialWord == null)
-    {
-      throw new ArgumentNullException(nameof(initialWord));
-    }
-    else if (initialWord.Length == 0)
-    {
-      throw new ArgumentException("Initial word shouldn't be empty", nameof(initialWord));
-    }
-
-    var sb = new StringBuilder(initialWord);
+    var sb = new StringBuilder(initialWord).Append(' ');
     string currentWord = initialWord;
     try
     {
       for (int i = 0; i < new Random().Next(5, 30); i++)
       {
-        currentWord = GetNextWord(currentWord);
-        sb.Append(' ').Append(currentWord);
+        currentWord = index.RandomNextToken(currentWord);
+        sb.Append(currentWord).Append(' ');
       }
     }
     catch (ArgumentException)
@@ -113,25 +64,87 @@ public class MarkovChainSentences
       Console.WriteLine("Failed to get the next word. Will end the sentence immediately.");
     }
 
-    sb.Append(' ').Append(GetSentenceEnding()).Append('.');
+    sb.Append(index.RandomSentenceTerminatingToken()).Append(".");
 
     // Capitalize sentence
     sb[0] = char.ToUpper(sb[0]);
     return sb;
   }
+}
 
-  private string GetNextWord(string initialWord)
+class Index
+{
+  // token -> list of all tokens that appear after the token
+  // todo: deal with the duplicates, it would be nice to have a weighted list instead
+  // e.g., https://en.wikipedia.org/wiki/Alias_method, https://github.com/cdanek/KaimiraWeightedList?tab=readme-ov-file
+  private readonly Dictionary<string, List<string>> bigrams = new();
+  private readonly List<string> sentenceTerminators = new List<string>();
+
+  // TODO: Index a directory or a file
+  public void IndexFile(string filename)
   {
-    if (!bigrams.TryGetValue(initialWord, out var possibleWords))
-    {
-      throw new ArgumentException("Failed to get words to continue");
-    }
+    using var sr = new StreamReader(filename);
+    var content = sr.ReadToEnd();
 
-    return possibleWords[random.Next(possibleWords.Count)];
+    IndexBigrams(content);
+    IndexSentenceTerminatingTokens(content);
   }
 
-  private string GetSentenceEnding()
+  /// <summary>
+  /// Create a word -> list of words mapping. When "predicting" the next token, we will sample from that list.
+  /// </summary>
+  /// <param name="content"></param>
+  private void IndexBigrams(string content)
   {
-    return endWords[random.Next(endWords.Count)];
+    var tokens = Tokenizer.Tokenize(content);
+    for (int i = 0; i < tokens.Count - 1; i++)
+    {
+      var current = tokens.ElementAt(i);
+      var next = tokens.ElementAt(i + 1);
+      
+      var adjacentTokens = bigrams.GetValueOrDefault(current, new List<string>());
+      adjacentTokens.Add(next);
+      bigrams[current] = adjacentTokens;
+    }
+  }
+
+  private void IndexSentenceTerminatingTokens(string content)
+  {
+    // find words right before sentence terminating characters
+    Regex rx = new(@"\b(?<word>\w+)[!.?;]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+    MatchCollection matches = rx.Matches(content);
+
+    sentenceTerminators.AddRange(matches.Select(match => match.Groups["word"].Value));
+  }
+
+  public string RandomSentenceTerminatingToken() => sentenceTerminators.RandomItem();
+
+  public string RandomToken() => bigrams.RandomItem().Key;
+
+  public string RandomNextToken(string token) {
+    if (!bigrams.TryGetValue(token, out var candidates))
+    {
+      throw new ArgumentException($"There are no bigrams for {token}");
+    }
+    return candidates.RandomItem();
+  }
+}
+
+static class Tokenizer
+{
+  private static string[] wordSeparators = new string[] { "!", "\"", "#", "$", "%", "&", "'", ",", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", "(", ")", " ", Environment.NewLine };
+
+  public static ICollection<string> Tokenize(string text)
+  {
+    return text.ToLower().Split(wordSeparators, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+  }
+}
+
+static class Extensions 
+{
+  private readonly static Random random = new();
+  public static T RandomItem<T>(this ICollection<T> collection)
+  {
+    return collection.ElementAt(random.Next(collection.Count));
   }
 }
