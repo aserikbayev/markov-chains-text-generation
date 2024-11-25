e18846ff18d342f83d09901c72a6b4c425b993e4
Azat Serikbayev <125980868+aserikbayev@users.noreply.github.com>
2024-11-25 15:32:51 +0500

    ♻️ Better readability

--- Program.cs
+++ Program.cs
@@ -1,13 +1,9 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace MarkovChainSentences;
 
-public class TextGenerator
+public class Program
 {
-  private readonly static Random random = new();
-  private Index index = new Index();
-
   public static void Main()
   {
     var generator = TextGenerator.From("input.txt");
@@ -16,59 +12,72 @@ public class TextGenerator
 
     Console.WriteLine(text);
   }
+}
+
+public class TextGenerator
+{
+  private readonly Random _random = new();
+  private readonly Index _index = new();
+
+  private const int MinSentenceWords = 5;
+  private const int MaxSentenceWords = 20;
+  private const int MinSentencesPerParagraph = 2;
+  private const int MaxSentencesPerParagraph = 10;
 
   public static TextGenerator From(string path)
   {
+    if (!File.Exists(path))
+    {
+      throw new FileNotFoundException("Input file not found", path);
+    }
+
     var generator = new TextGenerator();
-    generator.index.IndexFile(path);
+    generator._index.IndexFile(path);
     return generator;
   }
 
-  public StringBuilder GenerateParagraphs(int min = 1, int? max = default, string? initialWord = default)
+  public string GenerateParagraphs(int min = 1, int? max = default, string? initialWord = default)
   {
     max ??= min;
-    var sb = new StringBuilder();
-    for (int i = 0; i < random.Next(min, max.Value); i++)
-    {
-      sb.Append(GenerateParagraph(initialWord)).Append(Environment.NewLine).Append(Environment.NewLine);
-    }
-    return sb;
+
+    var paragraphs = Enumerable.Range(0, _random.Next(min, max.Value))
+        .Select(_ => GenerateParagraph(initialWord));
+
+    return string.Join(Environment.NewLine + Environment.NewLine, paragraphs);
   }
 
-  public StringBuilder GenerateParagraph(string? initialWord = default)
+  public string GenerateParagraph(string? initialWord = default)
   {
-    var sb = new StringBuilder();
-    for (int i = 0; i < random.Next(2, 10); i++)
-    {
-      sb.Append(GenerateSentence(initialWord)).Append(' ');
-    }
-    return sb;
+    var sentences = Enumerable.Range(MinSentencesPerParagraph, MaxSentencesPerParagraph + 1)
+      .Select(_ => GenerateSentence(initialWord));
+
+    return string.Join(' ', sentences);
   }
 
-  public StringBuilder GenerateSentence(string? initialWord = default)
+  public string GenerateSentence(string? initialWord = default)
   {
-    initialWord ??= index.RandomToken();
+    initialWord ??= _index.RandomToken();
+    var words = new List<string> { initialWord };
 
-    var sb = new StringBuilder(initialWord).Append(' ');
     string currentWord = initialWord;
-    try
+    var sentenceLength = _random.Next(MinSentenceWords, MaxSentenceWords + 1);
+    for (int i = 0; i < sentenceLength; i++)
     {
-      for (int i = 0; i < new Random().Next(5, 30); i++)
+      try
       {
-        currentWord = index.RandomNextToken(currentWord);
-        sb.Append(currentWord).Append(' ');
+        currentWord = _index.RandomNextToken(currentWord);
+        words.Add(currentWord);
+
+      }
+      catch (ArgumentException)
+      {
+        break; // End sentence early if no next token is found
       }
     }
-    catch (ArgumentException)
-    {
-      Console.WriteLine("Failed to get the next word. Will end the sentence immediately.");
-    }
 
-    sb.Append(index.RandomSentenceTerminatingToken()).Append(".");
-
-    // Capitalize sentence
-    sb[0] = char.ToUpper(sb[0]);
-    return sb;
+    words.Add(_index.RandomSentenceTerminatingToken() + ".");
+    words[0] = char.ToUpper(words[0][0]) + words[0][1..]; // Capitalize sentence
+    return string.Join(" ", words);
   }
 }
 
@@ -77,15 +86,13 @@ class Index
   // token -> list of all tokens that appear after the token
   // todo: deal with the duplicates, it would be nice to have a weighted list instead
   // e.g., https://en.wikipedia.org/wiki/Alias_method, https://github.com/cdanek/KaimiraWeightedList?tab=readme-ov-file
-  private readonly Dictionary<string, List<string>> bigrams = new();
-  private readonly List<string> sentenceTerminators = new List<string>();
+  private readonly Dictionary<string, List<string>> _bigrams = new();
+  private readonly List<string> _sentenceTerminators = new List<string>();
 
   // TODO: Index a directory or a file
   public void IndexFile(string filename)
   {
-    using var sr = new StreamReader(filename);
-    var content = sr.ReadToEnd();
-
+    var content = File.ReadAllText(filename);
     IndexBigrams(content);
     IndexSentenceTerminatingTokens(content);
   }
@@ -101,50 +108,40 @@ class Index
     {
       var current = tokens.ElementAt(i);
       var next = tokens.ElementAt(i + 1);
-      
-      var adjacentTokens = bigrams.GetValueOrDefault(current, new List<string>());
+
+      var adjacentTokens = _bigrams.GetValueOrDefault(current, new List<string>());
       adjacentTokens.Add(next);
-      bigrams[current] = adjacentTokens;
+      _bigrams[current] = adjacentTokens;
     }
   }
 
   private void IndexSentenceTerminatingTokens(string content)
   {
     // find words right before sentence terminating characters
-    Regex rx = new(@"\b(?<word>\w+)[!.?;]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-    MatchCollection matches = rx.Matches(content);
-
-    sentenceTerminators.AddRange(matches.Select(match => match.Groups["word"].Value));
-  }
-
-  public string RandomSentenceTerminatingToken() => sentenceTerminators.RandomItem();
-
-  public string RandomToken() => bigrams.RandomItem().Key;
-
-  public string RandomNextToken(string token) {
-    if (!bigrams.TryGetValue(token, out var candidates))
+    var rx = new Regex(@"\b(?<word>\w+)[!.?;]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+    foreach(Match match in rx.Matches(content))
     {
-      throw new ArgumentException($"There are no bigrams for {token}");
+      _sentenceTerminators.Add(match.Groups["word"].Value);
     }
-    return candidates.RandomItem();
   }
+
+  public string RandomSentenceTerminatingToken() => _sentenceTerminators.RandomItem();
+  public string RandomToken() => _bigrams.RandomItem().Key;
+  public string RandomNextToken(string token) => 
+    _bigrams.GetValueOrDefault(token)?.RandomItem() 
+      ?? throw new ArgumentException($"No bigrams found for {token}");
 }
 
 static class Tokenizer
 {
-  private static string[] wordSeparators = new string[] { "!", "\"", "#", "$", "%", "&", "'", ",", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", "(", ")", " ", Environment.NewLine };
+  private static readonly char[] WordSeparators = "!\"#$%&'*+,-./:;<=>?@[\\]^_`{|}~() \r\n\t".ToCharArray();
 
-  public static ICollection<string> Tokenize(string text)
-  {
-    return text.ToLower().Split(wordSeparators, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-  }
+  public static ICollection<string> Tokenize(string text) => text.ToLower()
+      .Split(WordSeparators, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 }
 
-static class Extensions 
+static class Extensions
 {
   private readonly static Random random = new();
-  public static T RandomItem<T>(this ICollection<T> collection)
-  {
-    return collection.ElementAt(random.Next(collection.Count));
-  }
+  public static T RandomItem<T>(this ICollection<T> collection) => collection.ElementAt(random.Next(collection.Count));
 }
